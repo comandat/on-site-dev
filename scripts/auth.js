ed55081fb63fe86d22259f41ed214d2094ec69d8
comandat <contact@comandat.ro>
2025-11-27 14:23:28 +0200

    Delete scripts directory

--- scripts/auth.js
+++
@@ -1,9 +0,0 @@
-// scripts/auth.js
-
-// Acest script se va rula pe paginile protejate
-// Verifica daca utilizatorul este autentificat. Daca nu, il redirectioneaza la login.
-if (sessionStorage.getItem('isLoggedIn') !== 'true') {
-    // Pastreaza pagina curenta pentru o eventuala redirectionare dupa login
-    // sessionStorage.setItem('redirectUrl', window.location.href);
-    window.location.href = 'index.html';
-}
